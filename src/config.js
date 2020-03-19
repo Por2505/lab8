f2ab8051291cabad662f129e2d7806f5e57e6154
Por2505 <s6035512061@phuket.psu.ac.th>
2020-03-19 23:25:15 +0700

    fist

---
+++ src/config.js
@@ -0,0 +1,13 @@
+
+const config = {
+    apiKey: "AIzaSyCdnDD5w-IQAGQuV-txbHcV7xuVrgPhaqE",
+    authDomain: "todo061.firebaseapp.com",
+    databaseURL: "https://todo061.firebaseio.com",
+    projectId: "todo061",
+    storageBucket: "todo061.appspot.com",
+    messagingSenderId: "751746203344",
+    appId: "1:751746203344:web:18b8acad5416e291a128d5",
+    measurementId: "G-847RF942V6"
+  };
+
+  export default config
